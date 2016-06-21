2b432838d48cc07d98228004cd9604903dd756b5
mariomac <mariomac@gmail.com>
2016-06-21 12:23:16 +0200

    Tests now run on docker

--- runtestEnv.sh
+++ runtestEnv.sh
@@ -2,12 +2,21 @@
 
 echo "Running mongodb..."
 
-cd etc/db
-
-mongod -f mongo.conf &
+docker run --name appmon-mongo -p 27017:27017 -d mongo:2.6
 
 echo "Running activeMQ..."
 
-cd ../activemq/bin
+docker run --name appmon-activemq \
+	-p 5672:5672 \
+	-p 61616:61616 \
+	-e 'ACTIVEMQ_ENABLED_AUTH=false' \
+	-d webcenter/activemq
 
-./activemq start
+
+
+#	-e 'ACTIVEMQ_WRITE_LOGIN=guest' \
+#	-e 'ACTIVEMQ_WRITE_PASSWORD=guest' \
+#	-e 'ACTIVEMQ_READ_LOGIN=guest' \
+#	-e 'ACTIVEMQ_READ_PASSWORD=guest' \
+#	-e 'ACTIVEMQ_JMX_LOGIN=guest' \
+#	-e 'ACTIVEMQ_JMX_PASSWORD=guest' \
