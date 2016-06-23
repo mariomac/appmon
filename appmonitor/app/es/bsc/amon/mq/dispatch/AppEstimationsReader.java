962ddf40e68eac10d52176fc0787cc7432c963b3
mariomac <mariomac@gmail.com>
2016-06-23 16:06:41 +0200

    new version of app monitor

--- appmonitor/app/es/bsc/amon/mq/dispatch/AppEstimationsReader.java
+++ appmonitor/app/es/bsc/amon/mq/dispatch/AppEstimationsReader.java
@@ -2,8 +2,6 @@ package es.bsc.amon.mq.dispatch;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.*;
-import es.bsc.amon.controller.EventsDBMapper;
-import es.bsc.amon.model.Event;
 import play.Logger;
 
 import javax.jms.*;
@@ -11,12 +9,18 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class AppEstimationsReader {
 
 	boolean running = true;
 	Thread theThread;
+
+	// 8 Apr 2016 08:57:56 GMT
+	public static final DateFormat emDateFormat = new SimpleDateFormat("dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
+	public static final TimeZone emTimeZone = TimeZone.getTimeZone("GMT");
 
 	public void stop() {
 		running = false;
@@ -43,18 +47,33 @@ public class AppEstimationsReader {
 							try {
 								TextMessage tm = (TextMessage) clientTopic.receive(5000);
 //						{"lakjsf":null,"provider":"00000","applicationid":"maximTestApp","deploymentid":"938","vms":["5699"],"unit":"WATTHOUR","generattiontimestamp":"8 Apr 2016 08:57:56 GMT","referredtimestamp":"8 Apr 2016 08:57:56 GMT","value":48.36}
+//								To: {"ApplicationId":"SinusApp","Timestamp":1431592067367,"Terms":{"metric":9.862471417356321}}
 								if (tm != null) {
 									System.out.println("received message: " + tm.getText());
 
-									ObjectNode estimation = (ObjectNode) new ObjectMapper().readTree(tm.getText());
-									ObjectNode asEvent = JsonNodeFactory.instance.objectNode();
-									asEvent.set(EventsDBMapper.APPID, estimation.get("applicationid"));
-									asEvent.set(EventsDBMapper.DEPLOYMENT_ID, estimation.get("deploymentid"));
-									ObjectNode data = JsonNodeFactory.instance.objectNode();
-									data.set("energyEstimation", estimation.get("value"));
-									asEvent.set(EventsDBMapper.DATA, data);
+									ObjectNode estimationFromEM = (ObjectNode) new ObjectMapper().readTree(tm.getText());
+									ObjectNode metricToSLAM = JsonNodeFactory.instance.objectNode();
+									metricToSLAM.set(SLAM_APPID, estimationFromEM.get(EM_APPID));
+									metricToSLAM.set(SLAM_DEPLOYMENTID, estimationFromEM.get(EM_DEPLOYMENTID));
 
-									submitEstimation(asEvent);
+									long timestamp = System.currentTimeMillis();
+									String dateStr = null;
+									try {
+										dateStr = estimationFromEM.get(EM_REFERREDTIMESTAMP).asText();
+										Calendar c = GregorianCalendar.getInstance(emTimeZone);
+										c.setTime(emDateFormat.parse(dateStr));
+										timestamp = c.getTimeInMillis();
+									} catch(Exception e) {
+										Logger.warn("Unable to parse the referred timestamp: " + dateStr + ". Proceeding with current timestamp as default",e);
+									}
+
+									metricToSLAM.set(SLAM_TIMESTAMP,new LongNode(timestamp));
+
+									ObjectNode terms = JsonNodeFactory.instance.objectNode();
+									terms.set(SLAM_METRIC, estimationFromEM.get(EM_VALUE));
+									metricToSLAM.set(SLAM_TERMS, terms);
+
+									submitEstimation(metricToSLAM);
 								}
 								//EventsDBMapper.INSTANCE.storeEvent(asEvent);
 							} catch (Exception e) {
@@ -95,9 +114,9 @@ public class AppEstimationsReader {
 		}
 	}
 
-	private void submitEstimation(ObjectNode event) throws JMSException, IOException, NamingException {
-		String appId = event.get(EventsDBMapper.APPID).asText();
-		String deploymentId = event.get(EventsDBMapper.DEPLOYMENT_ID).asText();
+	private void submitEstimation(ObjectNode metricToSlam) throws JMSException, IOException, NamingException {
+		String appId = metricToSlam.get(SLAM_APPID).asText();
+		String deploymentId = metricToSlam.get(SLAM_DEPLOYMENTID).asText();
 
 		String topicName = TOPIC_PREFIX + appId + "." + deploymentId  + TOPIC_SUFFIX;
 		String topicKey = "topic." + appId + deploymentId;
@@ -115,8 +134,8 @@ public class AppEstimationsReader {
 		TopicSession session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
 
 		Logger.debug("Submitting estimation to topic: " + topicName);
-		Logger.debug("Submitting next estimation: " + event.toString());
-		TextMessage estimationMessage = session.createTextMessage(event.toString());
+		Logger.debug("Submitting next estimation: " + metricToSlam.toString());
+		TextMessage estimationMessage = session.createTextMessage(metricToSlam.toString());
 		final Topic topic = (Topic) context.lookup(appId+deploymentId);
 		session.createProducer(topic).send(estimationMessage);
 	}
@@ -129,6 +148,18 @@ public class AppEstimationsReader {
 		return TOPIC_PREFIX + appId + "." + deploymentId + TOPIC_SUFFIX;
 
 	}
+
+	private static final String EM_APPID = "applicationid";
+	private static final String EM_DEPLOYMENTID = "deploymentid";
+	private static final String EM_REFERREDTIMESTAMP = "referredtimestamp";
+	private static final String EM_VALUE = "value";
+
+	// {"ApplicationId":"SinusApp","Timestamp":1431592067367,"Terms":{"metric":9.862471417356321}}
+	private static final String SLAM_APPID = "ApplicatinId";
+	private static final String SLAM_DEPLOYMENTID = "DeploymentId";
+	private static final String SLAM_TIMESTAMP = "Timestamp";
+	private static final String SLAM_TERMS = "Terms";
+	private static final String SLAM_METRIC = "metric";
 
 	private static final String TOPIC_PREFIX = "application-monitor.monitoring.";
 	private static final String TOPIC_SUFFIX = ".estimation";
